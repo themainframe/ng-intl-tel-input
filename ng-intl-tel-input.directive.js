3e985df1bf15a71fdff423e208890c8f3eec2c06
Reinis Pakers <rpakers@kasko.io>
2018-05-17 14:55:15 +0300

    PROD-1887: added zero

--- ng-intl-tel-input.directive.js
+++ ng-intl-tel-input.directive.js
@@ -27,7 +27,7 @@ angular.module('ngIntlTelInput')
                 (ctrlDown && $event.keyCode === 67) ||
                 (ctrlDown && $event.keyCode === 86) ||
                 (ctrlDown && $event.keyCode === 88) ||
-                [1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number($event.key)))) {
+                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number($event.key)))) {
                 $event.stopPropagation();
                 $event.preventDefault();
               }
